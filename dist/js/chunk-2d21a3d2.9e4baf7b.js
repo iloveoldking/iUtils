e3fca16c33fea289eac7a2c4cf10f1a2eb29a421
wangcong <wangcong@fsmeeting.com>
2019-07-19 11:36:08 +0800

    build test

--- dist/js/chunk-2d21a3d2.9e4baf7b.js
+++ dist/js/chunk-2d21a3d2.9e4baf7b.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21a3d2"],{bb51:function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",{staticClass:"home"},[n._v("\n  home\n")])},s=[],c={name:"home"},l=c,o=t("2877"),u=Object(o["a"])(l,a,s,!1,null,null,null);e["default"]=u.exports}}]);
-//# sourceMappingURL=chunk-2d21a3d2.7a9f9ea7.js.map
+//# sourceMappingURL=chunk-2d21a3d2.9e4baf7b.js.map
