02a6abfda44241184162eb9db08a77c4b5c1ad02
king <862567329@qq.com>
2020-01-04 13:42:02 +0800

    修改dispatch解释

--- src/components/UmiConnect.vue
+++ src/components/UmiConnect.vue
@@ -2,6 +2,7 @@
   <div>
     <pre>
       // connect会把dispatch自动挂载到组件的this.props上，state通过connect函数挂载
+      // 注意dispatch一个action是异步操作，会返回一个promise对象，可以用then回调，dispatch一个reducer是同步操作，不会返回promise对象，也就是不可以使用then
       import { connect } from 'dva';
       @connect(({ user }) => ({ user }))
       class BlankLayout extends Component {
